184e78b02d710e766cf6621d3e1074a844035e62
Surya Prashanth <prashantsurya002@gmail.com>
2025-04-15 16:56:42 +0530

    refactor getVersion to get package version

--- src/common/version.ts
+++ src/common/version.ts
@@ -1,11 +1,5 @@
-import { readFileSync } from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import pkg from "../../package.json" with { type: "json" };
 
 export function getVersion() {
-  const packageJson = JSON.parse(readFileSync(join(__dirname, "..", "..", "package.json"), "utf8"));
-  return packageJson.version;
+  return pkg.version;
 }
