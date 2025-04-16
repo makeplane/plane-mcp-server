1d6ee64dc9125bf38b074318d1ebc07be90bf83c
Surya Prashanth <prashantsurya002@gmail.com>
2025-04-16 15:02:11 +0530

    fix: projects api serialization (#3)

--- src/tools/projects.ts
+++ src/tools/projects.ts
@@ -2,10 +2,38 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { z } from "zod";
 
 import { makePlaneRequest } from "../common/request-helper.js";
+import { type Project } from "../schemas.js";
+
+type ProjectsResponse = {
+  grouped_by: null;
+  sub_grouped_by: null;
+  total_count: number;
+  next_cursor: string;
+  prev_cursor: string;
+  next_page_results: boolean;
+  prev_page_results: boolean;
+  count: number;
+  total_pages: number;
+  total_results: number;
+  extra_stats: null;
+  results: Project[];
+};
 
 export const registerProjectTools = (server: McpServer) => {
   server.tool("get_projects", "Get all projects for the current user", {}, async () => {
-    const projects = await makePlaneRequest("GET", `workspaces/${process.env.PLANE_WORKSPACE_SLUG}/projects`);
+    const projectsResponse: ProjectsResponse = await makePlaneRequest<ProjectsResponse>(
+      "GET",
+      `workspaces/${process.env.PLANE_WORKSPACE_SLUG}/projects/`
+    );
+
+    const projects = projectsResponse.results.map((project) => ({
+      name: project.name,
+      id: project.id,
+      identifier: project.identifier,
+      description: project.description,
+      project_lead: project.project_lead,
+    }));
+
     return {
       content: [
         {
@@ -21,10 +49,12 @@ export const registerProjectTools = (server: McpServer) => {
     "Create a new project",
     {
       name: z.string().describe("The name of the project"),
+      identifier: z.string().max(7).describe("The identifier of the project. This is typically a word of around 5 characters derived from the name of the project in uppercase."),
     },
-    async ({ name }) => {
-      const project = await makePlaneRequest("POST", `workspaces/${process.env.PLANE_WORKSPACE_SLUG}/projects`, {
+    async ({ name, identifier }) => {
+      const project = await makePlaneRequest("POST", `workspaces/${process.env.PLANE_WORKSPACE_SLUG}/projects/`, {
         name,
+        identifier: identifier.toUpperCase().replaceAll(" ", ""),
       });
       return {
         content: [
